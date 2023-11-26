7010475b1fde0a7ed0392dbe8d5e6187b4b2006c
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-11-26 17:59:08 -0300

    Aumento dos testes.

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -20,4 +20,20 @@ public class TestCalculadora {
         double retornoFeito = calculadora.getDiferenca();
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
+
+    @Test
+    void testGetProduto() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+    @Test
+    void testGetQuociente() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getQuociente();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
 }
